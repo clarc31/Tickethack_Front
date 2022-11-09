1bff84758e797d0711f78a8aa871b2d41d3c92a4
Helene Zieba <helene.zieba@free.fr>
2022-11-09 17:11:48 +0100

    sauvegarde 17h11

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/* fetch('http://localhost:3000/trips') 
-    .then(response => response.json()) 
-    .then(data => { */
-        
 document.querySelector('#btn-search').addEventListener('click', function() {
     const departure = document.querySelector('#departure').value;
     const arrival = document.querySelector('#arrival').value;
@@ -9,13 +5,15 @@ document.querySelector('#btn-search').addEventListener('click', function() {
     if (arrival && departure && calendar) {
         fetch('http://localhost:3000/trips', {
             method: 'POST',
-            hearders: {'Content-Type': 'application/json'},
-            body: JSON.stringify({departure, arrival, calendar})
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({departure: departure, arrival: arrival, date: calendar})
         }).then(response => response.json())
         .then(data => {
-            if (data.result) {
+            {
                 console.log(data)
             }
         })
     }
 })
+
+// How to convert array to objet ?
