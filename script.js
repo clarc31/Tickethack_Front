f143f0ea825bad933ec126157d8b9e45f3f1cfc5
Helene Zieba <helene.zieba@free.fr>
2022-11-09 13:00:24 +0100

    sauvegarde 13h

---
+++ script.js
@@ -0,0 +1,24 @@
+/* fetch('http://localhost:3000/trips') 
+    .then(response => response.json()) 
+    .then(data => { */
+        
+document.querySelector('#btn-tickethack').addEventListener('click',
+
+function () {
+ console.log('Click detected on TicketHack!');
+}
+);
+
+document.querySelector('#btn-cart').addEventListener('click',
+
+function () {
+ console.log('Click detected on Cart!');
+}
+);
+
+document.querySelector('#btn-bookings').addEventListener('click',
+
+function () {
+ console.log('Click detected on Bookings!');
+}
+)/*}*/; 
