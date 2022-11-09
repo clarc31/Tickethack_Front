b57de0ad3d8d68495c6a8844ac7ed49070f9b81b
Helene Zieba <helene.zieba@free.fr>
2022-11-09 14:51:23 +0100

    sauvegarde de la branche front 3 hmtl et 3 css + 1 script

--- script.js
+++ script.js
@@ -2,23 +2,20 @@
     .then(response => response.json()) 
     .then(data => { */
         
-document.querySelector('#btn-tickethack').addEventListener('click',
-
-function () {
- console.log('Click detected on TicketHack!');
-}
-);
-
-document.querySelector('#btn-cart').addEventListener('click',
-
-function () {
- console.log('Click detected on Cart!');
-}
-);
-
-document.querySelector('#btn-bookings').addEventListener('click',
-
-function () {
- console.log('Click detected on Bookings!');
-}
-)/*}*/; 
+document.querySelector('#btn-search').addEventListener('click', function() {
+    const departure = document.querySelector('#departure').value;
+    const arrival = document.querySelector('#arrival').value;
+    const calendar = document.querySelector('#calendar').value;
+    if (arrival && departure && calendar) {
+        fetch('http://localhost:3000/trips', {
+            method: 'POST',
+            hearders: {'Content-Type': 'application/json'},
+            body: JSON.stringify({departure, arrival, calendar})
+        }).then(response => response.json())
+        .then(data => {
+            if (data.result) {
+                console.log(data)
+            }
+        })
+    }
+})
